e4597aa1c4a2329c5f86eb943e707129d32cb41a
mamamama <maks.denishchuk@gmail.com>
2023-07-21 18:00:10 +0300

    update transaction form

--- components/WalletForm.tsx
+++ components/WalletForm.tsx
@@ -2,28 +2,56 @@ import { FC } from "react";
 
 export const WalletForm: FC = () => {
   return (
-    <div className="bg-white p-10 flex flex-col justify-center items-center gap-8 border border-gray-300 rounded-xl shadow-sm">
-      <div className="bg-blue-500 h-8 py-2 px-4 flex items-center justify-center text-white font-semibold rounded-md">
-        Transaction form
+    <main className="relative py-3 sm:max-w-xl sm:mx-auto">
+      <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="max-w-md mx-auto">
+          <div>
+            <h2 className="text-2xl font-semibold">Transaction form</h2>
+          </div>
+          <div className="divide-y divide-gray-200">
+            <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              <div className="relative">
+                <input
+                  autoComplete="off"
+                  id="address"
+                  name="address"
+                  type="text"
+                  className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                  placeholder="Wallet address"
+                />
+                <label
+                  htmlFor="address"
+                  className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                >
+                  Wallet Address
+                </label>
+              </div>
+              <div className="relative">
+                <input
+                  autoComplete="off"
+                  id="amount"
+                  name="amount"
+                  type="text"
+                  className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                  placeholder="Coin amount"
+                />
+                <label
+                  htmlFor="amount"
+                  className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                >
+                  Coin Amount
+                </label>
+              </div>
+              <div className="relative">
+                <button className="bg-blue-500 text-white rounded-md px-4 py-1">
+                  Send
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <form className="flex flex-col justify-center items-center gap-4 w-1/2">
-        <input
-          type="text"
-          placeholder="Wallet address"
-          className="bg-gray-100 w-full px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
-        <input
-          type="text"
-          placeholder="Amount"
-          className="bg-gray-100 w-full px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 w-full text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-600 transition-colors"
-        >
-          Send
-        </button>
-      </form>
-    </div>
+    </main>
   );
 };
