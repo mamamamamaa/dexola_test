edd287ff8f28344fe4560f2af8d209eb7d685c06
mamamama <maks.denishchuk@gmail.com>
2023-07-25 11:30:09 +0300

    fix up

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,10 @@
 import { FC } from "react";
-import { useAccount } from "wagmi";
 import { Web3Button } from "@web3modal/react";
 import { Logo } from "@/components/Logo/Logo";
-import { Loader } from "@/components/Loader/Loader";
 
 import style from "./Header.module.css";
 
 export const Header: FC = () => {
-  const { isReconnecting } = useAccount();
-
-  if (isReconnecting) return <Loader />;
-
   return (
     <header className={style.header}>
       <Logo />
